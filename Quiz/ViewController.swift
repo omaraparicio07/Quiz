434f4082aeaa4f5868be82336c6ac5736bed431f
Omar <omaraparicio07@gmail.com>
2019-10-26 15:38:19 -0500

    Template first application IOS, and binding labels storyboard to view controller

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel : UILabel!
+    @IBOutlet var answerLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showNextQuestion(_ sender:UIButton){
+        
+    }
+    
+    @IBAction func showAnswer(_ sender:UIButton ){
+        
+    }
 
 }
 
